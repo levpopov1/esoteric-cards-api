2db2a1ea8d4e92a80fb045e8d074c481c2718973
Lev Popov <levpopov1@gmail.com>
2021-01-06 17:22:39 +0000

    renames model file

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var PersonSchema = new Schema({
+var CategoriesSchema = new Schema({
     firstname: {
         type: String,
         required: true
@@ -17,4 +17,4 @@ var PersonSchema = new Schema({
 });
 
 
-module.exports = mongoose.model('Person', PersonSchema, 'people');
+module.exports = mongoose.model('Category', CategoriesSchema, 'categories');
