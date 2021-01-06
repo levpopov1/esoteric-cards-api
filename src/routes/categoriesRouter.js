b8e0674ee4a6607f659af3f3d11c6d5c40c3970a
Lev Popov <levpopov1@gmail.com>
2021-01-06 17:21:16 +0000

    renames router file

---
+++ src/routes/categoriesRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const categoriesController = require('../controllers/categoriesController');
+const Category = mongoose.model('Category');
+
+const findByID = require('../middleware/findById');
+const findAll = require('../middleware/findAll');
+
+// all categories
+router.get('/', findAll(Category), categoriesController.getAllCategories);
+router.get('/:id', findByID(Category), categoriesController.getOneCategory);
+
+// vondors in a category
+router.get('/:id/vendors', findByID(Category), categoriesController.getAllVendors);
+// router.get('/:id/vendors/:id', findByID(Category), findByID(Category), categoriesController.getOneVendor);
+
+// decks in a category
+router.get('/:id/decks', findByID(Category), categoriesController.getAllDecks);
+// router.get('/:id/:deck_id', CategorysRouter);
+
+// write
+router.post('/', categoriesController.post);
+
+
+module.exports = router;
