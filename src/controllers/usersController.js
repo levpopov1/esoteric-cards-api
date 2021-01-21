e5e7020fa6d4cbb4e0b6557967f1b0c680c263c8
Lev Popov <levpopov1@gmail.com>
2021-01-21 00:57:00 +0000

    adds user controller

---
+++ src/controllers/usersController.js
@@ -0,0 +1,38 @@
+const User = require('../models/user');
+const API = require('../middleware/apiKeys');
+
+function getAllUsers(req, res){
+    res.status(200).json(req.items);
+}
+
+function getOneUser(req, res){
+    res.status(200).json(req.item);
+}
+
+async function registerUser(req, res, next){
+    let origin = req.headers.host;
+
+    let acc = {
+        api_key: API.generateKey(),
+        host: origin,
+        usage: {
+            requests: 0,
+            lastAccessTime: Date.now()
+        }
+    }
+    
+    try {
+        const newUser = await User.create(acc);
+        res.status(201).json(newUser);
+    } 
+    catch (error) {
+        next(error);
+    }
+}
+
+
+module.exports = {
+    getAllUsers,
+    getOneUser,
+    registerUser,
+}
