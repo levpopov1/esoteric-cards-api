44753e4f5f7f0c8bf630f4b7c5b3553befee82b0
Lev Popov <levpopov1@gmail.com>
2021-06-14 23:48:39 +0100

    rename response message to error

--- src/middleware/errorHandlers.js
+++ src/middleware/errorHandlers.js
@@ -8,7 +8,7 @@ function notFound(req, res, next) {
 function genericErrorHandler(err, req, res, next) {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
     res.status(statusCode).json({
-        message: err.message,
+        error: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     });
 }
