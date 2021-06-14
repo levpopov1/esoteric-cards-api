b4bf604ad39c6b78e4ffe8876bbd598a4df69d6c
Lev Popov <levpopov1@gmail.com>
2021-06-14 23:48:09 +0100

    add auth router file

---
+++ src/routes/authRotuer.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const authController = require('../controllers/authController');
+const User = mongoose.model('User');
+
+const findByID = require('../middleware/findById');
+const findAll = require('../middleware/findAll');
+const { validateKey } = require('../middleware/apiKeys');
+const isAuthenticated = require('../middleware/isAuthenticated');
+
+// all users
+router.post('/login', authController.login);
+router.post('/register',  authController.register);
+router.post('/refresh_token', isAuthenticated,  authController.refreshToken);
+router.post('/logout', isAuthenticated,  authController.logout);
+router.get('/secret',  isAuthenticated,  authController.secret);
+
+module.exports = router;
