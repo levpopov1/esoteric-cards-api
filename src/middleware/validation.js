bbc9ccddd9c34df6edea618ebd489deb1df2fcb5
Lev Popov <levpopov1@gmail.com>
2021-06-15 17:34:40 +0100

    add validation middleware

---
+++ src/middleware/validation.js
@@ -0,0 +1,27 @@
+const { body } = require('express-validator');
+const User = require('../models/user');
+
+
+function validate(target){
+  switch (target) {
+    case 'createUser':
+      return [
+        body('email', 'Missing email').exists(),
+        body('email', 'Invalid email').isEmail(),
+        body('password', 'Missing password').exists(),
+        body('password', 'Invalid password length, must be between 16 and 64 characters').isLength({min: 16, max: 64}),
+        body('username').optional().isLength({min: 3, max: 32}),
+        body('email').custom(async function checkUserExists(value){
+          const userExists = await User.findOne({email: value});
+          if(userExists){
+            return Promise.reject('Email already in use');
+          }
+          return Promise.resolve();
+        })
+      ];
+    default:
+      return false;
+  }
+}
+
+module.exports = validate;
